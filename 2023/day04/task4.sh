f6306b8b78cebabb1901ee4e9491c50ba1864e75
prajyotigokhare <prajyoti.gokhare@fineshift.com>
2024-07-22 22:37:33 +0530

    day4 challenge complete

---
+++ 2023/day04/task4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="Prajyoti"
+echo "Enter your sirname"
+read sirname
+echo "Your First and Last name is" $name $sirname
+
+echo  $1  "is your sister?"
+
+#pass argument while running the file .
+#ex ./task4.sh Gayatri
+
